19e83d973dac8302f1a93ea0cfa817c7d5292120
willdev-crypto <wil.dgl@hotmail.com>
2023-05-22 13:37:06 -0300

    project created

---
+++ angular-player/src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.css', './menu-bar.responsivo.component copy.css']
+})
+export class MenuBarComponent {
+
+}
